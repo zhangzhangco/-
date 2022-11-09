d1e7a3e0463ce5fce8f8320b4ada383a15753281
zhangzhangco <zhangxin78@gmail.com>
2022-11-09 10:05:17 +0800

    增加2016复审工作信息

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
+app.get('/',function(req,res){
+    res.send(fs.readFileSync('I:\\WPSDrive\\259626762\\WPS网盘\\标准研究室\\mediastandards-registry-china\\build\\index.html','utf-8'));
+})
 app.use(function(req,res){
-   res.end('hello,node!');
-});
+    res.send('hello world');
+})
 app.listen(3000);
 console.log('server running !');
