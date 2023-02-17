989d9806e54563829f39329bcd41150926963077
zhangzhangco <zhangxin78@gmail.com>
2023-02-17 11:29:35 +0800

    use docker

--- src/server.js
+++ src/server.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-app.get('/',function(req,res){
-    res.send(fs.readFileSync('I:\\WPSDrive\\259626762\\WPS网盘\\标准研究室\\mediastandards-registry-china\\build\\index.html','utf-8'));
-})
-app.use(function(req,res){
-    res.send('hello world');
-})
+app.use('/', express.static('./build'))
 app.listen(3000);
 console.log('server running !');
