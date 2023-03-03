b6320700d391ce412c699b78b5ec35632b86d922
zhangzhangco <zhangxin78@gmail.com>
2023-03-03 10:50:56 +0800

    科研质检所参与的国标31，行标34；去除重复标准；改进pdf文件名搜索，使用字符串相似度

--- src/main/scripts/tmp/parseJS.py
+++ src/main/scripts/tmp/parseJS.py
@@ -1,6 +1,6 @@
 import jsonpath
 import json
-import sys, os, fnmatch
+import sys, os, fnmatch, difflib
 from shutil import copyfile
 
 def find(pattern, path):
@@ -11,6 +11,16 @@ def find(pattern, path):
                 result.append(os.path.join(root, name))
     return result
 
+def compare_files(files, s):
+    max_similarity = 0
+    max_file = None
+    for file in files:
+        similarity = difflib.SequenceMatcher(None, file, s).quick_ratio()
+        if similarity > max_similarity:
+            max_similarity = similarity
+            max_file = file
+    return max_file
+
 docs = "/src/main/data/documents.json"
 #docs = "\\src\\main\\data\\documents.json"
 with open(os.path.abspath('.')+docs, encoding='utf8') as f:
@@ -20,10 +30,10 @@ with open(os.path.abspath('.')+docs, encoding='utf8') as f:
 for key in range(len(data)):
     if jsonpath.jsonpath(data, "$["+str(key)+"].href"):
         file = data[key]['docTitle']
-        fn = find('*'+file+'*.pdf', '/Users/John/Library/Containers/com.kingsoft.wpsoffice.mac/Data/Library/Application Support/Kingsoft/WPS Cloud Files/userdata/qing/filecache/John的云文档/标准研究室/标准处文件共享/文献资料/标准电子版归档')
-        #fn = find('*'+file+'*.pdf', 'I:\\WPSDrive\\259626762\\WPS网盘\\标准研究室\\标准处文件共享\\文献资料\\标准电子版归档')
-        if len(fn) > 0:
-            srcfile = fn[0]
+        #fn = find('*'+file+'*.pdf', '/Users/John/Library/Containers/com.kingsoft.wpsoffice.mac/Data/Library/Application Support/Kingsoft/WPS Cloud Files/userdata/qing/filecache/John的云文档/标准研究室/标准处文件共享/文献资料/标准电子版归档')
+        fn = find('*'+file+'*.pdf', 'I:\\WPSDrive\\259626762\\WPS网盘\\标准研究室\\标准处文件共享\\文献资料\\标准电子版归档')
+        if len(fn) > 0:            
+            srcfile = compare_files(fn, data[key]['docLabel'].replace('/', '')+' '+data[key]['docTitle'])
             objfilename = data[key]['docLabel'].replace('/', '')+' '+data[key]['docTitle']+'.pdf'
             objfilename = objfilename.replace(' ', '_')
             objfile = os.path.abspath('../')+'/filmstandards/'+objfilename
@@ -36,3 +46,4 @@ for key in range(len(data)):
 with open(os.path.abspath('.')+docs, 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False, indent=2)
 print(json.dumps(data, ensure_ascii=False))
+
